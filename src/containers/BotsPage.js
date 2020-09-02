0a6de20b961f7136f5eb2b2d9e29f478ed34bdc1
Hyung Kim <hjkmines@gmail.com>
2020-09-01 21:05:28 -0600

    adds comments

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import BotCollection from "./BotCollection";
 import Army from "./YourBotArmy";
 import BotSpecs from "../components/BotSpecs";
 
-class BotsPage extends React.Component {
+class BotsPage extends Component {
+  //Set State for the robot army 
   state = {
-    bots: [],
+    bots: [], //lists all the bots from the API fetch 
     loading: true,
-    army: [],
-    selected: null
+    army: [], //List of chosen robots for the army 
+    selected: null //set default to no selection when first rendering 
   };
 
+  //fetch for the robots after initial render to the bots array, set the loading to initially set to false 
   componentDidMount() {
     fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
       .then(response => response.json())
@@ -21,6 +23,7 @@ class BotsPage extends React.Component {
     this.setState({ selected });
   };
 
+  //Add chosen robot to the army, if it does exist, they add it twice
   handleArmyClick = bot => {
     let army = [...this.state.army];
     army = army.filter(Bot => Bot.id !== bot.id);
